b635c282db33bbea7c97cf990e4c11d7fb5352ec
Josh Klun <jmklun@gmail.com>
2015-02-03 22:35:34 -0600

    Make duplicate checks look at related words.

--- src/main/scala/JournalGenerator.scala
+++ src/main/scala/JournalGenerator.scala
@@ -12,6 +12,8 @@ class JournalGenerator(val sourceTitles: Iterable[String]) {
     Set("and", "&") -> "and"
   )
 
+  private def suffixes = Seq("s", "es", "ing", "ed")
+
   private def normalizeWord(sourceWord: String): String = {
     val lowerSourceWord = sourceWord.toLowerCase
     val matchingRules = normalizeWordRules.keys.filter(_.contains(lowerSourceWord))
@@ -25,9 +27,8 @@ class JournalGenerator(val sourceTitles: Iterable[String]) {
       nameBuffer += indexFrequencyMap(i).foldLeft(("", 0)) {
         case ((oldWord, oldWordCount), (newWord, newWordCount)) =>
           if (newWordCount > oldWordCount &&
-            !nameBuffer.contains(newWord)
-            && (i < newWordCount - 1 || !noEndWords.contains(newWord))
-          ) {
+            duplicateCheck(nameBuffer, newWord) &&
+            endWordCheck(newWord, i, wordCount)) {
             (newWord, newWordCount)
           } else {
             (oldWord, oldWordCount)
@@ -37,10 +38,28 @@ class JournalGenerator(val sourceTitles: Iterable[String]) {
     nameBuffer.map(_.capitalize).mkString(" ")
   }
 
+  private def endWordCheck(newWord: String, currentIndex: Int, wordCount: Int): Boolean = {
+    currentIndex < wordCount - 1 || !noEndWords.contains(newWord)
+  }
+
+  private def duplicateCheck(currentWords: mutable.Buffer[String], newWord: String): Boolean = {
+    !currentWords.exists {
+      currentWord =>
+        val potentialSuffix = currentWord match {
+          case word if word.length > newWord.length => Some(word.substring(newWord.length))
+          case word if word.length < newWord.length => Some(newWord.substring(word.length))
+          case word if word.length == newWord.length => None
+        }
+        potentialSuffix.fold(currentWords.contains(newWord))(suffixes.contains(_))
+    }
+  }
+
   private def buildIndexedFrequencyMap: Seq[Map[String, Int]] = {
     val indexFrequencyMap = mutable.Buffer[mutable.Map[String, Int]]()
     val splitTitles = sourceTitles.map(_.split("\\s+"))
-    val maxTitleWords = splitTitles.foldLeft(0)((oldMax, words) => math.max(oldMax, words.length))
+    val maxTitleWords = splitTitles.foldLeft(0) {
+      (oldMax, words) => math.max(oldMax, words.length)
+    }
     (0 to maxTitleWords - 1).foreach {
       wordColumn => indexFrequencyMap += mutable.Map[String, Int]()
     }
